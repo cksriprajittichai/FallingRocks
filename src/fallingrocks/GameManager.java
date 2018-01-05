a1f890df6469c2182d7aa3ba06ba8ebe6cd31703
cksriprajittichai <sriprajittichai@gmail.com>
2018-01-05 02:29:21 -0800

    Added pause feature, score display, and GameOverPanel which allows in-application game restarts after a death.

---
+++ src/fallingrocks/GameManager.java
@@ -0,0 +1,101 @@
+package fallingrocks;
+
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class GameManager extends JFrame implements ActionListener {
+
+	private static final long serialVersionUID = 1L;
+	private boolean gameRunning;
+	private JPanel topPanel;
+	private GamePanel gamePanel;
+	private LightUpPanel lightUpPanel;
+	private GameOverPanel gameOverPanel;
+	private Timer timer;
+
+	private GameManager() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setMinimumSize(new Dimension(800, 800));
+		setMaximumSize(new Dimension(800, 800));
+		setResizable(false);
+		setLocationRelativeTo(null);
+
+		gameRunning = false;
+
+		topPanel = new JPanel();
+		topPanel.setMinimumSize(new Dimension(800, 800));
+		topPanel.setMaximumSize(new Dimension(800, 800));
+
+		gamePanel = null;
+		lightUpPanel = null;
+		gameOverPanel = null;
+
+		timer = null;
+	}
+
+	// Initializes gamePanel and lightUpPanel, and adds them to a top-level
+	// container, and starts game.
+	private void startNewGame() {
+		gameRunning = true;
+
+		KeyConverter listener = new KeyConverter();
+		gamePanel = new GamePanel(listener);
+		lightUpPanel = new LightUpPanel(listener);
+
+		topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.Y_AXIS)); // GPanel on top of bPanel
+		topPanel.add(gamePanel);
+		topPanel.add(lightUpPanel);
+		topPanel.revalidate();
+		topPanel.addKeyListener(listener); // Must have
+
+		getContentPane().add(topPanel);
+		setVisible(true);
+
+		topPanel.requestFocusInWindow(); // Not required if keyListener is added to frame instead of topPanel
+
+		timer = new Timer(5, null);
+		timer.addActionListener(gamePanel);
+		timer.addActionListener(lightUpPanel);
+		timer.addActionListener(this);
+		timer.start();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+		if (gameRunning == true && gamePanel.getGameOverStatus() == true) { // If the game is running but
+																			// should be stopped
+			gameRunning = false;
+
+			timer.removeActionListener(gamePanel);
+			timer.removeActionListener(lightUpPanel);
+			gamePanel = null;
+			lightUpPanel = null;
+
+			topPanel.removeAll();
+			gameOverPanel = new GameOverPanel();
+			topPanel.add(gameOverPanel);
+			topPanel.revalidate();
+
+		} else if (gameRunning == false) {
+			if (gameOverPanel.isPlayAgainPressed() == true) {
+				topPanel.removeAll();
+				gameOverPanel = null;
+				startNewGame();
+			}
+		}
+
+	}
+
+	public static void main(String args[]) {
+		GameManager gm = new GameManager();
+		gm.startNewGame();
+	}
+
+}
